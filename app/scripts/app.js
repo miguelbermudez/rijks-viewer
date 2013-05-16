3baed9133b435fd919ea0e2e2d5c7cc1bb9a59f6
Miguel Bermudez <miguel.bermudez@controlgroup.com>
2013-05-16 18:08:09 -0400

    updated links for work details and back to old school hash routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,14 @@
 
 angular.module('rijksViewerApp', ['infinite-scroll'])
   .config(function ($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        reloadOnSearch: false
+        controller: 'MainCtrl'
+        //reloadOnSearch: false
       })
-      .when('/:id', {
+      .when('/detail/:id', {
         templateUrl: 'views/detail.html',
         controller: 'DetailCtrl'
       })
