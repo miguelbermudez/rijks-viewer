bc1203feafdb66fa91eeec8f85b1e156ce2b9681
Miguel Bermudez <gmiggs@gmail.com>
2013-05-31 03:01:20 -0400

    refactored worktile  directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,15 +4,6 @@ angular.module('rijksViewerApp')
   .controller('MainCtrl', function ($scope, $http, $location, $routeParams, $log, Api) {
     $scope.works = [];
 
-    $scope.set_background = function (item) {
-      var backgroundImage, backgroundSize, backgroundPosition;
-
-      backgroundImage = 'background: url("' + Api.getImageUrl(item.work_id) + '") no-repeat 0 0;';
-      backgroundSize = 'background-size: cover;';
-      backgroundPosition = 'background-position: 50%';
-      return backgroundImage + backgroundSize + backgroundPosition;
-    };
-
 
     $scope.loadMore = function () {
       $log.debug('routeaparams: ', $routeParams);
--- app/scripts/directives/workTile.js
+++ app/scripts/directives/workTile.js
@@ -1,14 +1,29 @@
 'use strict';
 
 angular.module('rijksViewerApp')
-  .directive('mbWorkTile', function () {
+  .directive('mbWorktile', function () {
     return {
-      //template: '<div></div>',
-      restrict: 'A',
+      template: '<li><a href="/#/detail/{{workitem.work_id | lowercase}}"'
+                  + 'style="{{set_background(workitem)}}" '
+                  + 'title="{{workitem.title}} by {{workitem.creator}}" '
+                  + 'class="works_tile" '
+                  + 'data-identifier="{{workitem.identifier}}">'
+                  +'</a>'
+                  + '<div class="id-card">'
+                  + '<h2>{{workitem.title | truncate:60}}</h2>'
+                  + '<p>{{workitem.creator}}</p><p class="date">{{workitem.date}}</p>'
+                  + '</div></li>',
+      restrict: 'AE',
+      replace: true,
+      scope: {
+        workitem: '='
+      },
       link: function postLink(scope, element) {
-        var idCard, parentUl;
+        var idCard, parentUl, anchorImage;
         idCard = angular.element(element.children()[1]);
         parentUl = angular.element(element.parent());
+        anchorImage = angular.element(element.find('a'));
+        console.log(anchorImage.css('background'));
         element.bind('mouseover', function() {
           parentUl.children().addClass('deactive');
           idCard.parent().addClass('active').toggleClass('deactive');
@@ -19,6 +34,16 @@ angular.module('rijksViewerApp')
           idCard.parent().removeClass('active');
           idCard.removeClass('over');
         });
+      },
+      controller: function($scope, $element, $attrs, $location, Api) {
+        $scope.set_background = function (item) {
+          var backgroundImage, backgroundSize, backgroundPosition;
+
+          backgroundImage = 'background: url("' + Api.getImageUrl(item.work_id) + '") no-repeat 0 0;';
+          backgroundSize = 'background-size: cover;';
+          backgroundPosition = 'background-position: 50%';
+          return backgroundImage + backgroundSize + backgroundPosition;
+        };
       }
     };
   });
